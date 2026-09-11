d23e2887d47cb70589e4b722a519b0893cdf6f8e
sobhalakshmiprasanna25csm-sys <sobhalakshmiprasanna.25.csm@anits.edu.in>
2026-09-11 11:19:44 +0530

    Create static.java

---
+++ Assignment 2/static.java
@@ -0,0 +1,18 @@
+class Student {
+
+static int count=50;
+
+void display(){
+
+System.out.println(count);
+}
+}
+public class Main {
+
+public static void main(String[]args){
+
+Student s=new Student();
+
+s.display();
+}
+}
