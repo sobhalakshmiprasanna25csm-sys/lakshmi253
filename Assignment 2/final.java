12e5955813e67f4cd22940aca9c0cc4719bda819
sobhalakshmiprasanna25csm-sys <sobhalakshmiprasanna.25.csm@anits.edu.in>
2026-09-11 11:12:30 +0530

    Create final.java

---
+++ Assignment 2/final.java
@@ -0,0 +1,17 @@
+class Student {
+final int marks=70;
+
+void display (){
+System.out.println(marks);
+
+}
+}
+
+public class Main {
+
+public static void main(Sting[]args){
+
+Student s=new Student();
+s.display():
+}
+}
